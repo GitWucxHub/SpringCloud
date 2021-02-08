8b1838bb709519e68653c0e725a575aae0f9a7a4
GitWucxHub <737860160@qq.com>
2021-02-08 21:07:34 +0800

    commit

---
+++ cloud-consumerfeign-order8083/src/main/java/com/alphago/springcloud/CloudConsumerfeignOrder8083Application.java
@@ -0,0 +1,13 @@
+package com.alphago.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConsumerfeignOrder8083Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConsumerfeignOrder8083Application.class, args);
+    }
+
+}
---
+++ cloud-consumerfeign-order8083/src/test/java/com/alphago/springcloud/CloudConsumerfeignOrder8083ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphago.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConsumerfeignOrder8083ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
