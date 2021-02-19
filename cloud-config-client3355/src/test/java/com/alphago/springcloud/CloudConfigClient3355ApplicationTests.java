058e54d4d0515582b06fba4f3d35c0492e75cbd7
GitWucxHub <737860160@qq.com>
2021-02-19 23:33:25 +0800

    commit

---
+++ cloud-config-client3355/src/test/java/com/alphago/springcloud/CloudConfigClient3355ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphago.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigClient3355ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
